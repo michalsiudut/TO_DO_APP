4dfb0e9236692f896ef2130ae9e1c62ae9e077c1
michalsiudut <michal.siudut.ms@gmail.com>
2025-08-10 20:58:15 +0200

    Setting up Tanstack Router

---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import './index.css'
+import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('root');
+if (rootElement && !rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+      <App></App>
+    </StrictMode>,
+  )
+}
